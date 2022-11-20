02347b86384f8f451a64fc0ac94dd25061814871
cb-pintukumar <99131869+cb-pintukumar@users.noreply.github.com>
2022-11-20 13:08:51 +0530

    editted

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("testing");
-    res.send('<h1>Testing</h1>');
-});
+const requestTime = function (req, res, next) {
+    console.log('hello pp')
+    req.requestTime = Date.now()
+    next()
+  }
+  
+app.use(requestTime)
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
 
 app.listen(4000);
